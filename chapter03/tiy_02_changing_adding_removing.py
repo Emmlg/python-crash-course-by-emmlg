ac5c1125a7c5e07fef647f9d19ff7c316e4a5c0a
Emmanuel Lopez <emmanuelg.dev@gmail.com>
2025-08-12 10:16:07 -0600

    missing one exceersice

--- chapter03/tiy_02_changing_adding_removing.py
+++ chapter03/tiy_02_changing_adding_removing.py
@@ -47,3 +47,27 @@ guest_list.insert(-1,"Gabriela") # inser as penultimate
 guest_list.append("Hugo")
 
 print(guest_list)
+
+# 3-7. Shrinking Guest List: You just found out that your new dinner table won’t 
+# arrive in time for the dinner, and you have space for only two guests.
+# •	 Start with your program from Exercise 3-6. Add a new line that prints a 
+# message saying that you can invite only two people for dinner.
+# •	 Use pop() to remove guests from your list one at a time until only two 
+# names remain in your list. Each time you pop a name from your list, print 
+# a message to that person letting them know you’re sorry you can’t invite 
+# them to dinner.
+# •	 Print a message to each of the two people still on your list, letting them 
+# know they’re still invited.
+# •	 Use del to remove the last two names from your list, so you have an empty 
+# list. Print your list to make sure you actually have an empty list at the end 
+# of your program
+
+print("sorry guys, something come up, i can invite only 2 people")
+del guest_list[0]
+del guest_list[0]
+
+guest_list.remove("Hugo")
+guest_list.remove("Gabriela")
+
+print("the last person i remove is : " ,guest_list.pop())
+print(guest_list)
